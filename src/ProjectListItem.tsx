e6ef412cecc8ac858e3c678e629506e02930c90c
Tristan K <tristanmkernan@gmail.com>
2019-07-07 15:05:06 -0400

    feat: timeline from bulma extensions

--- src/ProjectListItem.tsx
+++ src/ProjectListItem.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Project, Tag } from "./types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const StyledParagraph = styled.p`
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+`;
 
 type TagProps = {
   tag: Tag;
@@ -16,26 +22,21 @@ type Props = {
 
 const ProjectListItem: React.FC<Props> = ({ project }: Props) => {
   return (
-    <article className="media">
-      <figure className="media-left">
-        <p className="image is-64x64">
-          <img alt="project image" src={project.image} />
-        </p>
-      </figure>
-      <div className="media-content">
-        <div className="content">
-          <p>
-            <strong>{project.name}</strong> <small>{project.date}</small>
-            <br />
-            {project.copy}
-          </p>
-        </div>
-        <div className="level is-mobile">
-          <div className="tags">
-            {project.tags.map(tag => (
-              <TagItem key={tag.name} tag={tag} />
-            ))}
-          </div>
+    <div className="timeline-item is-primary">
+      <div className="timeline-marker is-primary is-image is-32x32">
+        <img src={project.image} />
+      </div>
+      <div className="timeline-content">
+        <p className="heading">{project.date}</p>
+        <StyledParagraph>
+          <strong>{project.name}</strong>
+          <br />
+          {project.copy}
+        </StyledParagraph>
+        <div className="tags">
+          {project.tags.map(tag => (
+            <TagItem key={tag.name} tag={tag} />
+          ))}
         </div>
         <nav className="level is-mobile">
           <div className="level-left">
@@ -49,7 +50,7 @@ const ProjectListItem: React.FC<Props> = ({ project }: Props) => {
           </div>
         </nav>
       </div>
-    </article>
+    </div>
   );
 };
 
